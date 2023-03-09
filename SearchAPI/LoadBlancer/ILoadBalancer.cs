39242fd9bcd6125d43863479f71aa3d400684e9c
hlynur92 <hlynur92@live.com>
2023-03-09 11:03:17 +0100

    LoadBalancer Interfaces

---
+++ SearchAPI/LoadBlancer/ILoadBalancer.cs
@@ -0,0 +1,12 @@
+﻿namespace SearchAPI.LoadBlancer
+{
+    public interface ILoadBalancer
+    {
+        public List<string> GetAllServices();
+        public int AddServices(string url);
+        public int RemoveServices(int id);
+        public ILoadBalancerStrategy GetActiveStrategy();
+        public void SetActiveStrategy(ILoadBalancerStrategy strategy);
+        public string NextService();
+    }
+}
---
+++ SearchAPI/LoadBlancer/ILoadBalancerStrategy.cs
@@ -0,0 +1,7 @@
+﻿namespace SearchAPI.LoadBlancer
+{
+    public interface ILoadBalancerStrategy
+    {
+        public string NextService(List<string> services);
+    }
+}
