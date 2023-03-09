35d52c4dc2c0f01db2b7adb41c9ef9cd4b5737a0
hlynur92 <hlynur92@live.com>
2023-03-09 11:02:08 +0100

    wwwroot

---
+++ WebSearch/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿let me = this;
+
+me.searchTerms = ko.observable();
+me.hits = ko.observable();
+me.results = ko.observableArray();
+me.timeUsed = ko.observable();
+
+me.search = function () {
+    $.ajax({
+        url: "http://localhost:5041/search?terms=" + me.searchTerms() + "&numberOfResults=10",
+        success: function (data) {
+            me.hits(data.documents.length);
+            me.timeUsed(data.elapsedMilliseconds);
+            me.results.removeAll();
+            data.documents.foreach(function (hit) {
+                me.results.push(hit);
+            });
+        }
+    });
+};
+ko.applyBindings(new ViewModel());
