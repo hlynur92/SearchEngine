b92c05ce083b00fce16c6d9845564000a1676257
hlynur92 <hlynur92@live.com>
2023-03-09 12:34:24 +0100

    round robin

--- LoadBalancer/Controllers/RoundRobin.cs
+++ LoadBalancer/Controllers/RoundRobin.cs
@@ -4,9 +4,22 @@ namespace LoadBalancer.Controllers
 {
     public class RoundRobin : ILoadBalancerStrategy
     {
+        int index = 0;
+        int count;
+
         public string NextService(List<string> services)
         {
-            throw new NotImplementedException();
+            count = services.Count;
+
+            if (index == count)
+            {
+                index = 0;
+            }
+
+            var useNext = index;
+            index++;
+
+            return services[useNext];
         }
     }
 }
