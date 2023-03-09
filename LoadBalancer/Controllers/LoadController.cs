cb84d5ac71245507e9b26cf9dce8827d392aa101
hlynur92 <hlynur92@live.com>
2023-03-09 13:26:33 +0100

    load balancer

---
+++ LoadBalancer/Controllers/LoadController.cs
@@ -0,0 +1,34 @@
+﻿using Common;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace LoadBalancer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoadController
+    {
+        LoadBalancer lb;
+
+        public LoadController(LoadBalancer lb)
+        {
+            this.lb = lb;
+        }
+
+        [HttpGet]
+        public string Search(string terms, int numberOfResults)
+        {
+            HttpClient api = new HttpClient();
+
+            api.BaseAddress = new Uri(lb.NextService());
+
+            var task = api.GetStringAsync("/Search?terms=" + terms + "&numberOfResults=" + numberOfResults);
+            task.Wait();
+
+            var result = task.Result;
+
+            return result;
+        }
+    }
+}
